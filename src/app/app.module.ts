00c3346019ac1b7dc007f0ad2f0ac58283dbf098
Aadam <aadamgafar@protonmail.com>
2022-11-26 20:26:28 +0000

    Added Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Component imports
 import { AppComponent } from './app.component';
+
+// Angular Material imports
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatChipsModule } from '@angular/material/chips'
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatSlideToggleModule,
+    MatToolbarModule,
+    MatChipsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
