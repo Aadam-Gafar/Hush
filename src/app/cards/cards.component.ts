4698ef15af922396960cd09779a669f8402566d2
Aadam <aadamgafar@protonmail.com>
2023-02-13 14:36:00 +0000

    Storing slider values

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
-
   constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  volumeBird: any;
+  volumeRain: any;
+  volumeCoffee: any;
+  volumePark: any;
+  volumeFire: any;
+  volumeStorm: any;
+
+  changeVolume(event: any, cardName: string) {
+    switch(cardName) {
+      case 'bird':
+        this.volumeBird = event.value;
+        break;
+      case 'rain':
+        this.volumeRain = event.value;
+        break;
+      case 'coffee':
+        this.volumeCoffee = event.value;
+        break;
+      case 'park':
+        this.volumePark = event.value;
+        break;
+      case 'fire':
+        this.volumeFire = event.value;
+        break;
+      case 'storm':
+        this.volumeStorm = event.value;
+        break;
+    }
   }
-
 }
